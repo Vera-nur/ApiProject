b1413ee9573dc5cce0e7e9f3b2ced2509a3144c9
Vera <135285088+Vera-nur@users.noreply.github.com>
2024-07-30 10:31:25 +0300

    Update database connection to use environment variables

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ const app = express();
 const port = 3000;
 
 const pool = new Pool({
-    user: 'admin',
-    host: 'localhost',
-    database: 'rahat_db',
-    password: '1234',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
 
